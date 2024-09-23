820f9d1907e5dc6d118b17d2d04296973c21ebcb
jeissonsp <jeissonperez1@gmail.com>
2024-09-23 17:01:55 -0500

    23 sep

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import useOrder from "./hooks/useOrder";
 import OrderContents from "./components/OrderContents";
 function App() {
 
-  const { order, addItem } = useOrder()
+  const { order, addItem, removeItem } = useOrder()
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -34,6 +34,7 @@ function App() {
           <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
             <OrderContents
             order={order}
+            removeItem = {removeItem}
             />
           </div>
         </div>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,5 +1,6 @@
 
 
+
 import { useState } from "react"
 import type { MenuItem, OrderItem } from "../types"
 
@@ -21,9 +22,12 @@ export default function useOrder(){
       setOrder([...order, newItem])
     }
   }
-  console.log(order)
+  const removeItem = (id:MenuItem['id'] ) =>{
+    setOrder(order.filter(item => item.id !== id))
+  }
   return{
     order,
-    addItem
+    addItem,
+    removeItem
   }
 }
